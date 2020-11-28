ffa57bcff8a51b69b24b1fa432f1e969054e9459
Jeff Daezon Almarez <izgreyleigh08@gmail.com>
2020-11-29 00:09:43 +0800

    final pop-up

--- beef.js
+++ beef.js
@@ -13,32 +13,41 @@ fetch(api)
         recipetitle.innerHTML = data.hits[i].recipe.label;
         const image = document.createElement("img");
         image.src = data.hits[i].recipe.image;
-        const popup = document.createElement("button");
-        popup.classList.add("popup");
-        popup.innerHTML = "ingredients";
-        const popuptext= document.createElement("span");
-        popuptext.classList.add("popuptext");
-        const ingredienthero2 =document.createElement("div");
-        ingredienthero2.classList.add("ingredienthero2");
-        /*const listofingre=document.createElement("li");
-        listofingre.classList.add("listofingre");
-        listofingre.innerHTML = data.hits[i].recipe.ingredientLines;*/
+        const button = document.createElement("button");
+        button.classList.add("btn-ingridients");
+        button.innerHTML = "ingredients";
 
-        
-
+    
         beefbox.appendChild(recipetitle);
         beefbox.appendChild(image);
-        beefbox.appendChild(popup);
-        beefbox.appendChild(span);
-        beefbox.appendChild(popuptext);
-        beefbox.appendChild(ingredienthero2);
-        beefbox.appendChild(listofingre);
+        beefbox.appendChild(button);
         beefpick.appendChild(beefbox);
 
         }
     });
 
-    function myFunction() {
-        const popup = document.getElementsByClassName("btn-ingridients");
-        popup.classList.toggle("show");
-      }
+    // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
